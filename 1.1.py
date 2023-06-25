9ad1563726d27a545dc7c5b79033d0bbc65d0c7a
KirillAltynov <136495823+KirillAltynov@users.noreply.github.com>
2023-06-25 20:28:10 +0300

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,7 @@
+name = input('Введите имя: ')
+surname = input('Введите фамилию: ')
+year = input('Введите год рождения: ')
+age = 2023-int(year)
+print(surname, end=' ')
+print(name, end=', ')
+print(age)
