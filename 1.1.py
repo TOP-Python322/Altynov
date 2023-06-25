fbab2ccec184d5a7cd1226ccd526eeed1fffa068
KirillAltynov <136495823+KirillAltynov@users.noreply.github.com>
2023-06-25 20:31:01 +0300

    Delete 1.1.py

--- 1.1.py
+++
@@ -1,7 +0,0 @@
-name = input('Введите имя: ')
-surname = input('Введите фамилию: ')
-year = input('Введите год рождения: ')
-age = 2023-int(year)
-print(surname, end=' ')
-print(name, end=', ')
-print(age)
